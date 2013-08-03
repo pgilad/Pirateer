784483f051c5bf66d8e9c3649b70ff4cd61b1354
Gilad Peleg <giladp007@gmail.com>
2013-08-03 22:47:55 +0300

    alpha working, need to continue to working beta

--- js/app.js
+++ js/app.js
@@ -1,6 +1,101 @@
 var app = angular.module('app', []);
 
-app.controller('FormCtrl', function( $scope) {
+app.run(['searchService', '$rootScope', function (searchService, $rootScope) {
+    var prop = {
+        id: 'getMovieData',
+        title: 'IMDB rating for %s',
+        contexts: ['selection']
+    };
 
-    $scope.hello = 'hello';
+    chrome.contextMenus.create(prop, function () {
+    });
+
+    // The onClicked callback function.
+    function onClickHandler(info, tab) {
+        if (info && info.searchText) {
+            $rootScope.$apply(function () {
+
+                searchService.searchText.title = info.searchText;
+            });
+        }
+    }
+
+    chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+}]);
+
+app.service('searchService', function () {
+    var searchText = {
+        title: ''
+    };
+
+    return {
+        searchText: searchText
+    }
 });
+
+app.controller('FormCtrl', ['$scope', '$http', 'searchService', function ($scope, $http, searchService) {
+
+    $scope.baseQueries = {};
+    $scope.baseQueries.getTitleURI = 'http://www.imdb.com/xml/find?json=1&nr=1&tt=on&mx=1&q=';
+    $scope.baseQueries.getRatingURI = 'http://p.media-imdb.com/static-content/documents/v1/title/AAA/ratings%3Fjsonp=imdb.rating.run:imdb.api.title.ratings/data.json';
+
+    $scope.populars = [];
+    $scope.subs = [];
+
+    $scope.getRating = function (item) {
+        var ratingURI = $scope.baseQueries.getRatingURI.replace('AAA', item.id);
+
+        $http.get(ratingURI)
+            .success(function (data) {
+                console.log('Rating Data-', data);
+                var _ratingData = data.substring('imdb.rating.run('.length);
+                _ratingData = JSON.parse(_ratingData.substring(0, _ratingData.length - 1));
+                item.rating = _ratingData['resource'] && +_ratingData['resource'].rating;
+            })
+            .error(function (err) {
+                console.log('Rating Err-', err);
+            })
+    };
+
+    $scope.text = function () {
+        return searchService.searchText.text;
+    };
+
+    searchService.searchText.text = 'hey';
+
+
+    $scope.searchIMDB = function (textToSearch) {
+        $scope.populars = [];
+        $scope.subs = [];
+
+        $http.get($scope.baseQueries.getTitleURI + encodeURI(textToSearch))
+            .success(function (data) {
+                var i = 0;
+
+                console.log('Id Data - ', data);
+
+                for (i = 0; data['title_popular'] && i < data['title_popular'].length && i < 3; ++i) {
+                    $scope.populars.push({
+                        id: data['title_popular'][i].id,
+                        title: data['title_popular'][i].title
+                    });
+
+                    //now get the rating
+                    $scope.getRating($scope.populars[i]);
+                }
+
+                for (i = 0; data['title_substring'] && i < data['title_substring'].length && i < 3; ++i) {
+                    $scope.subs.push({
+                        id: data['title_substring'][i].id,
+                        title: data['title_substring'][i].title
+                    });
+                    //now get the rating
+
+                    $scope.getRating($scope.subs[i]);
+                }
+            }).error(function (err) {
+                console.log('ID data-', err);
+            });
+    }
+}]);
