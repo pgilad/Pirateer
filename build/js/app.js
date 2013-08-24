e7f62ecbba8ece9482e1c0e9cc9ea392e3d55b0d
Gilad Peleg <giladp007@gmail.com>
2013-08-24 06:59:36 +0300

    minor

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,1 @@
-var app=angular.module("app",[]);app.run(["searchService","$rootScope",function(){}]),app.controller("FormCtrl",["$scope","$http","searchService",function(){}]);
+var app=angular.module("app",[]);
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,1 @@
 var app = angular.module('app', []);
-
-app.run(['searchService', '$rootScope', function (searchService, $rootScope) {
-
-}]);
-
-app.controller('FormCtrl', ['$scope', '$http', 'searchService', function ($scope, $http, searchService) {
-
-}]);
