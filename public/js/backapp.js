235a21cd31bf1027b1ca1f2d18f26eff18080beb
Gilad Peleg <giladp007@gmail.com>
2013-09-05 11:31:59 +0300

    added analytics to background page

--- public/js/backapp.js
+++ public/js/backapp.js
@@ -1,5 +1,12 @@
 var app = angular.module('app', []);
 
+var _gaq = _gaq || [];
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 app.run(['searchService', '$q', function (searchService, $q) {
 
     var shouldQuery = false;
@@ -59,6 +66,8 @@ app.run(['searchService', '$q', function (searchService, $q) {
             port.onMessage.addListener(function (msg) {
                 if (msg.type === 'list') {
                     shouldQuery = true;
+                    _gaq.push(['_setAccount', 'UA-43678943-3']);
+                    _gaq.push(['_trackPageview']);
                     onRequestHandler(msg.list, port, true);
                 }
             });
